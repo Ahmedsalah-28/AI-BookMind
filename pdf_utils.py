0e3339fe39fb5529bab793578244cbfc8a405c38
Ahmedsalah-28 <aahmedasd936@gmail.com>
2025-08-25 16:40:41 +0300

    pdf_utils.py

--- pdf_utils.py
+++ pdf_utils.py
@@ -13,7 +13,8 @@ def clean_text(text):
     """Remove page numbers, headers, and extra spaces"""
     text = re.sub(r"\n?\s*Page\s*\d+\s*\n?", "\n", text, flags=re.IGNORECASE)
     text = re.sub(r"\n{2,}", "\n", text)
-    text = re.sub(r"[^\S\r\n]{2,}", " ", text)  # إزالة المسافات المكررة
-    text = re.sub(r"\s+\n", "\n", text)  # إزالة المسافات قبل السطر الجديد
+    text = re.sub(r"[^\S\r\n]{2,}", " ", text)  
+    text = re.sub(r"\s+\n", "\n", text)  
 
     return text.strip()
+
